9f0899d8b224a1dda0b2fcca70727c06b60b341e
hai061898 <truonghaiauto98@gmail.com>
2021-06-28 10:28:18 +0700

    model

---
+++ lib/routes/routes_page.dart
@@ -0,0 +1,35 @@
+// import 'package:get/get.dart';
+
+abstract class RoutesName {
+  static const INITIAL = '/';
+  static const LOGIN = '/login';
+  static const WELCOME = '/welcome';
+  static const SIGNUP = '/signup';
+  static const HOME = '/home';
+  static const PAYMENTS = '/payments';
+  static const SCHEDULES = '/schedules';
+  static const RATINGS = '/ratings';
+  static const SETTINGS = '/settings';
+  static const COMPANY = '/company';
+  static const SERVICE = '/service';
+  static const EMPLOYEE_SCHEDULES = '/employee-schedules';
+}
+
+class RoutesPage {
+  static const INITIAL = RoutesName.INITIAL;
+   routes() => [
+    // GetPage(
+    //   name: RoutesName.INITIAL,
+    //   page: ()=>InitialView(),
+    //   binding: InitialBinding(),
+    // ),
+  ];
+}
+
+outes() => [
+    // GetPage(
+    //   name: RoutesName.INITIAL,
+    //   page: ()=>InitialView(),
+    //   binding: InitialBinding(),
+    // ),
+];
